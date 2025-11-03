02d3efc2d6b1150be610f4b1b798b2256c55dd37
Yunis <yunis@88.com>
2025-11-03 18:23:03 +0800

    fix synclient stuck

--- store/postgres/src/synclient.rs
+++ store/postgres/src/synclient.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
+
 use futures::executor::block_on;
 use sqlx::{
     Database, Error, IntoArguments, PgPool, Postgres,
     postgres::{PgPoolOptions, PgRow},
 };
-use std::time::Duration;
+use tokio::{runtime::Handle, task::block_in_place};
 
 #[derive(Debug, Clone)]
 pub struct SynClient {
@@ -13,13 +15,25 @@ pub struct SynClient {
 impl SynClient {
     pub fn connect(db_url: &str) -> Self {
         #[allow(clippy::expect_fun_call)]
-        let pool = block_on(async move {
-            PgPoolOptions::new()
-                .acquire_timeout(Duration::from_secs(5))
-                .max_connections(200)
-                .connect(db_url)
-                .await
-        })
+        let pool = if Handle::try_current().is_ok() {
+            block_in_place(|| {
+                block_on(async move {
+                    PgPoolOptions::new()
+                        .acquire_timeout(Duration::from_secs(5))
+                        .max_connections(200)
+                        .connect(db_url)
+                        .await
+                })
+            })
+        } else {
+            block_on(async move {
+                PgPoolOptions::new()
+                    .acquire_timeout(Duration::from_secs(5))
+                    .max_connections(200)
+                    .connect(db_url)
+                    .await
+            })
+        }
         .expect(&format!("failed to connect to DB {}", db_url));
 
         Self { pool }
@@ -29,30 +43,42 @@ impl SynClient {
     where
         A: IntoArguments<'q, Postgres> + 'q,
     {
-        block_on(async move {
-            let mut conn = self
-                .pool
-                .acquire()
-                .await
-                .expect("failed to get connection from pool");
+        if Handle::try_current().is_ok() {
+            block_in_place(|| {
+                block_on(async move {
+                    let mut conn = self.pool.acquire().await?;
 
-            sqlx::query_with(sql, params).fetch_one(&mut *conn).await
-        })
+                    sqlx::query_with(sql, params).fetch_one(&mut *conn).await
+                })
+            })
+        } else {
+            block_on(async move {
+                let mut conn = self.pool.acquire().await?;
+
+                sqlx::query_with(sql, params).fetch_one(&mut *conn).await
+            })
+        }
     }
 
     pub fn query<'q, A>(&self, sql: &'q str, params: A) -> Result<Vec<PgRow>, Error>
     where
         A: IntoArguments<'q, Postgres> + 'q,
     {
-        block_on(async move {
-            let mut conn = self
-                .pool
-                .acquire()
-                .await
-                .expect("failed to get connection from pool");
+        if Handle::try_current().is_ok() {
+            block_in_place(|| {
+                block_on(async move {
+                    let mut conn = self.pool.acquire().await?;
 
-            sqlx::query_with(sql, params).fetch_all(&mut *conn).await
-        })
+                    sqlx::query_with(sql, params).fetch_all(&mut *conn).await
+                })
+            })
+        } else {
+            block_on(async move {
+                let mut conn = self.pool.acquire().await?;
+
+                sqlx::query_with(sql, params).fetch_all(&mut *conn).await
+            })
+        }
     }
 
     pub fn execute<'q, A>(
@@ -63,29 +89,44 @@ impl SynClient {
     where
         A: IntoArguments<'q, Postgres> + 'q,
     {
-        block_on(async move {
-            let mut conn = self
-                .pool
-                .acquire()
-                .await
-                .expect("failed to get connection from pool");
+        if Handle::try_current().is_ok() {
+            block_in_place(|| {
+                block_on(async move {
+                    let mut conn = self.pool.acquire().await?;
 
-            sqlx::query_with(sql, params).execute(&mut *conn).await
-        })
+                    sqlx::query_with(sql, params).execute(&mut *conn).await
+                })
+            })
+        } else {
+            block_on(async move {
+                let mut conn = self.pool.acquire().await?;
+
+                sqlx::query_with(sql, params).execute(&mut *conn).await
+            })
+        }
     }
 
     pub fn batch_execute(&self, sqls: &[String]) -> Result<(), Error> {
-        block_on(async move {
-            let mut tx = self
-                .pool
-                .begin()
-                .await
-                .expect("failed to get connection from pool");
+        if Handle::try_current().is_ok() {
+            block_in_place(|| {
+                block_on(async move {
+                    let mut tx = self.pool.begin().await?;
 
-            for sql in sqls {
-                sqlx::query(sql).execute(&mut *tx).await?;
-            }
-            tx.commit().await
-        })
+                    for sql in sqls {
+                        sqlx::query(sql).execute(&mut *tx).await?;
+                    }
+                    tx.commit().await
+                })
+            })
+        } else {
+            block_on(async move {
+                let mut tx = self.pool.begin().await?;
+
+                for sql in sqls {
+                    sqlx::query(sql).execute(&mut *tx).await?;
+                }
+                tx.commit().await
+            })
+        }
     }
 }
